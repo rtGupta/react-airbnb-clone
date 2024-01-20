55e7fbb7187a53faa8937110c14cbf0c5f4d4b61
Aarti Gupta <rtgupta@Aartis-MacBook-Air.local>
2024-01-20 14:05:16 -0700

    Conditional dislay of badges

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@ import React from "react";
 import Card from "./components/Card";
 import ExperiencesHeader from "./components/ExperiencesHeader";
 import Navbar from "./components/Navbar";
-import data from "./data";
+import { data } from "./data";
 
 import "./styles.css";
 
 export default function App() {
-
-    const cards = data.map((item) => {
-        return (
-            <Card
-                img={item.coverImg}
-                rating={item.stats.rating}
-                reviewCount={item.stats.reviewCount}
-                location={item.location}
-                title={item.title}
-                price={item.price}
-            />
-        )
-    });
-
+  const cards = data.map((item) => {
     return (
-        <div>
-            <Navbar />
-            <ExperiencesHeader />
-            <section className="cards-carousel">
-                {cards}
-            </section>
-        </div>
+      <Card
+        key={item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+        openSpots={item.openSpots}
+        status={item.status}
+      />
     );
+  });
+
+  return (
+    <div>
+      <Navbar />
+      <ExperiencesHeader />
+      <section className="cards-carousel">{cards}</section>
+    </div>
+  );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,34 @@
 import React from "react";
 
 export default function Card(props) {
-    return (
-        <div className="card">
-            <img className="card-image" src={`images/${props.img}`} alt="Katie Zaferes" />
-            <div className="card-stats">
-                <img className="card-star" src="images/star.png" alt="star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
-            </div>
-            <h2 className="card-title">{props.title}</h2>
-            <p className="card-price"><b>From ${props.price}</b> / person</p>
-        </div>
-    )
+  let badgeText;
+
+  if (props.openSpots === 0) {
+    badgeText = "Sold Out";
+  } else if (props.status === "Online") {
+    badgeText = "Online";
+  }
+
+  return (
+    <div className="card">
+      {!!badgeText && (
+        <div className="card-badge">{badgeText.toUpperCase()}</div>
+      )}
+      <img
+        className="card-image"
+        src={`images/${props.img}`}
+        alt="Katie Zaferes"
+      />
+      <div className="card-stats">
+        <img className="card-star" src="images/star.png" alt="star" />
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.location}</span>
+      </div>
+      <h2 className="card-title">{props.title}</h2>
+      <p className="card-price">
+        <b>From ${props.price}</b> / person
+      </p>
+    </div>
+  );
 }
