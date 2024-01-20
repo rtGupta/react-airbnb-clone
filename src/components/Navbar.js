8ecc755ab56dc7e241cc6aefadb299b35abceec8
Aarti Gupta <rtgupta@Aartis-MacBook-Air.local>
2024-01-19 22:29:11 -0700

    Added the Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navabar() {
+    return (
+        <nav>
+            <img src="images/airbnb-logo.png" alt="Airbnb"></img>
+        </nav>
+    )
+}
